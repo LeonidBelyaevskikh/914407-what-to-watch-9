2cdbab4d43691f983bd4471ca8370d6134725a8f
leonid <belyaevskih.l@wildberries.ru>
2022-02-26 17:36:08 +0300

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,14 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+
 import Main from '../main/main';
+import SignIn from '../sing-in/sing-in';
+import MyList from '../my-list/my-list';
+import MoviePage from '../movie-page/movie-page';
+import AddReview from '../add-review/add-review';
+import Player from '../player/player';
+import NotFound from '../not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type Film = {
   name: string,
@@ -11,7 +21,46 @@ type AppProps = {
 };
 
 function App({promoFilm, films}: AppProps): JSX.Element {
-  return <Main promoFilm={promoFilm} films={films}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main promoFilm={promoFilm} films={films} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<MoviePage />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReview />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/components/not-found/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound(): JSX.Element {
+  return (
+    <section>
+      <h1>404 Not Found</h1>
+      <a href="/">Main page</a>
+    </section>
+  );
+}
+
+export default NotFound;
